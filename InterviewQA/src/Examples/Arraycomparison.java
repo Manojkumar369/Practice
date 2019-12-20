4f9ae229183b9c5ff2ed05bbf651faab1672fc9c
Manoj <Manoj@DESKTOP-10A811K>
2019-12-20 16:58:57 +0530

    Yes

--- InterviewQA/src/Examples/Arraycomparison.java
+++ InterviewQA/src/Examples/Arraycomparison.java
@@ -1,7 +1,20 @@
-Class ArrayComparison
+package Examples;
+
+import java.util.Arrays;
+
+class ArrayComparison
 {
 int a[]={1,2,3,4,5};
 int b[]={1,2,3,4,5};
 
-boolean flag=Arrays.eqaulsTo(a,b);
+boolean flag=Arrays.equals(a, b);
+{
+if(flag==true)
+{
+	System.out.println("arrays are same");
+}else {
+	System.out.println("arrays are not same");
+
+}
+}
 }
